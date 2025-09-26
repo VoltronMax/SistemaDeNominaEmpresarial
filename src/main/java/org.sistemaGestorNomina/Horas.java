37b884c42e206382753f88dce825f764a172c00b
Manuel Rodriguez <vmrodriguezd@ut.edu.co>
2025-09-26 18:22:09 -0500

    registro con tipos de hora, generalizo pa tirar logica ma rapido

---
+++ src/main/java/org.sistemaGestorNomina/Horas.java
@@ -0,0 +1,9 @@
+package org.sistemaGestorNomina;
+
+public enum Horas {
+    CORRIENTE,
+    EXTRA_DIURNA,
+    EXTRA_NOCTURNA,
+    EXTRA_DOMINICAL,
+    EXTRA_FESTIVO;
+}
--- src/main/java/org.sistemaGestorNomina/Registro.java
+++ src/main/java/org.sistemaGestorNomina/Registro.java
@@ -6,9 +6,11 @@ public class Registro {
 
     private LocalDate fechaRegistro;
     private String descripcion;
+    private Horas horas;
 
-    public Registro(LocalDate fechaRegistro, String descripcion){
+    public Registro(LocalDate fechaRegistro, String descripcion, Horas horas){
         this.fechaRegistro = LocalDate.now();
         this.descripcion = descripcion;
+        this.horas = horas;
     }
 }
