9901f677f9542131d417be16b3e72bbcfed5ecb6
Manuel Rodriguez <vmrodriguezd@ut.edu.co>
2025-09-28 02:11:03 -0500

    mrd

--- src/main/java/org.sistemaGestorNomina/Main.java
+++ src/main/java/org.sistemaGestorNomina/Main.java
@@ -6,24 +6,23 @@ public class Main {
 
         //prueba
         Empleado uwu = new Empleado("Pedro Lopez", "93130337", Cargo.EMPLEADO, 1_500_000);
-        Empleado a = new Empleado("Ashton Hall", "40772582", Cargo.ADMINISTRADOR, 3_000_000);
-
         Nomina n = new Nomina(uwu, Mes.ENERO);
-        Nomina e = new Nomina(a, Mes.DICIEMBRE);
 
-        a.setNominas(Mes.DICIEMBRE, e);
         uwu.setNominas(Mes.ENERO, n);
 
-
-
         GestorNomina ges = new GestorNomina();
-        ges.agregarEmpleado(a);
         ges.agregarEmpleado(uwu);
-        double res = ges.salarioConAuxilioDeTransporte("93130337", Mes.ENERO);
-        System.out.println(res);
-        System.out.println(ges.listarEmpleados());
+        ges.registrarHoras(uwu.getCedula(), 240, n.getMes());
+        ges.salarioOrdinarioBrutoPorMes(uwu.getCedula(), n.getMes());
+        ges.registrarBonificaciones(uwu.getCedula(), n.getMes(), 100_000, Bono.PREMIO);
+        ges.registrarHorasExtra(uwu.getCedula(), n.getMes(),10, Horas.EXTRA_NOCTURNA);
+        ges.calcularDeducciones(uwu.getCedula(), n.getMes());
+        ges.salarioConAuxilioDeTransporte(uwu.getCedula(), n.getMes());
 
+        double dx = n.getMontoBruto();
+        double m = ges.salarioOrdinarioBrutoPorMes(uwu.getCedula(), n.getMes());
 
+        System.out.println(m);
 
     }
 }
